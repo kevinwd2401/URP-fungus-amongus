9737eb496feaddb59015251d88c8c8af21817b7c
KevinDu10 <kevinwd2401@gmail.com>
2024-11-02 22:12:15 -0400

    Move Function for player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,12 +9,25 @@ public class Player : Character
     {
         
     }
-
     public void attack(PlayerAttacks attackEnum) {
         switch(attackEnum) {
         case PlayerAttacks.SlashRight:
             (Vector2 coord, int dmg)[] attackOffsets = {(new Vector2(1, 0), 3), (new Vector2(1, 1), 3), (new Vector2(1, -1), 3)};
             useAttack(new Attack("SlashRight", false, attackOffsets));
+            break;
+        default:
+            // code block
+            break;
+        }
+    }
+
+    public void moveMovement(MovementType movetype) {
+        switch(movetype) {
+        case MovementType.Left:
+            move(-1, 0);
+            break;
+        case MovementType.Right:
+            move(1, 0);
             break;
         default:
             // code block
@@ -28,6 +41,9 @@ public class Player : Character
     }
 }
 
+public enum MovementType {
+    Left, Right, Up, Down, UpLeft, DownLeft, UpRight, DownRight
+}
 public enum PlayerAttacks {
     SlashRight, SlashLeft
 }
