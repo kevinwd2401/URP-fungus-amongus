627a7f3bfbfbe83b2d084a0eff1589410740f721
nikong-penn <nikong@seas.upenn.edu>
2024-11-03 03:22:13 -0500

    new attacks

---
+++ Assets/Scripts/Skills/LunarSlash.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LunarSlash : Skill
+{
+    // Start is called before the first frame update
+
+
+    public LunarSlash()
+    {
+        // your attack
+        Vector2[] offsets = {
+            new Vector2(1, 0),
+            new Vector2(-1, 0),
+            new Vector2(0, 1),
+            new Vector2(0, -1),
+        };
+        // your attack area
+        (Vector2 coord, int dmg)[,] attackAreas = { // attackAreas.GetLength(0) = 4, attackAreas.GetLength(1) = area
+            { (new Vector2(1, 0), 2) , (new Vector2(2, 0), 2) , (new Vector2(3, 0), 2) },
+            { (new Vector2(-1, 0), 2) , (new Vector2(-2, 0), 2) , (new Vector2(-3, 0), 2) },
+            { (new Vector2(0, 1), 2) , (new Vector2(0, 2), 2) , (new Vector2(0, 3), 2) },
+            { (new Vector2(0, -1), 2) , (new Vector2(0, -2), 2) , (new Vector2(0, -3), 2) },
+        };
+
+        // attack initated by human
+        attack = new Attack("Lunar Slash", false, offsets, attackAreas);
+    }
+}
