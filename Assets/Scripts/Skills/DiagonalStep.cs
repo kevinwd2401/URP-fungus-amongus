4f3dbeb6e9629db7d295e5bc1430a95e59d88fb7
Sangjoon Lee <aaccjjt@hotmail.com>
2024-11-03 04:38:18 -0500

    horizontal diagonal works

---
+++ Assets/Scripts/Skills/DiagonalStep.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalStep : Skill
+{
+    // Start is called before the first frame update
+    public HorizontalStep()
+    {
+        // your move
+        Vector2[] offsets = {
+            new Vector2(1, 0),
+            new Vector2(-1, 0),
+            new Vector2(0, 1),
+            new Vector2(0, -1),
+        };
+        // your attack area
+        (Vector2 coord, int dmg)[,] attackAreas = { // attackAreas.GetLength(0) = 4, attackAreas.GetLength(1) = area
+            { (new Vector2(1, 0), 0) },
+            { (new Vector2(-1, 0), 0)},
+            { (new Vector2(0, 1), 0) },
+            { (new Vector2(0, -1), 0)},
+        };
+
+        isMove = true;
+        isAttack = false;
+
+        // attack initated by human
+        attack = new Attack("Horizontal Step", false, offsets, attackAreas);
+    }
+
+}
