6482f6114b3d3f2fec2f797f84fcd92da34ae380
KevinDu10 <kevinwd2401@gmail.com>
2024-11-02 21:48:24 -0400

    Player attack function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,9 +10,24 @@ public class Player : Character
         
     }
 
+    public void attack(PlayerAttacks attackEnum) {
+        switch(attackEnum) {
+        case PlayerAttacks.SlashRight:
+            (Vector2 coord, int dmg)[] attackOffsets = {(new Vector2(1, 0), 3), (new Vector2(1, 1), 3), (new Vector2(1, -1), 3)};
+            useAttack(new Attack("SlashRight", false, attackOffsets));
+            break;
+        default:
+            // code block
+            break;
+        }
+    }
 
     void Update()
     {
 
     }
+}
+
+public enum PlayerAttacks {
+    SlashRight, SlashLeft
 }
